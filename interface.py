ff80ad9cb2cad350571fb92df0ccc5bf8a5e8f37
Kanhaiyasaw <kanhaiyalal.wappnet@gmail.com>
2022-11-28 19:57:36 +0530

    All Task Addded

---
+++ interface.py
@@ -0,0 +1,24 @@
+
+from abc import *
+class DBInterface(ABC):
+   @abstractmethod
+   def connect(self):
+       pass
+   @abstractmethod
+   def disconnect(self):
+       pass
+class Oracle(DBInterface):
+   def connect(self):
+       print('Connecting to Oracle Database...')
+   def disconnect(self):
+       print('Disconnecting to Oracle Database...')
+class Sybase(DBInterface):
+   def connect(self):
+       print('Connecting to Sybase Database...')
+   def disconnect(self):
+       print('Disconnecting to Sybase Database...')   
+dbname=input('Enter Database Name either Oracle or Sybase:')
+classname=globals()[dbname]
+x=classname()
+x.connect()
+x.disconnect()
